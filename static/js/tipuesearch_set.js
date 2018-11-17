35870f4ca179aaf12ab369a3430c3d5b71a0ef23
Praxis | Darian <hello@vulpinecat.com>
2018-11-17 13:08:15 +0100

    adding tipuesearch components

---
+++ static/js/tipuesearch_set.js
@@ -0,0 +1,11 @@
+
+/*
+Tipue Search 4.0
+Copyright (c) 2014 Tipue
+Tipue Search is released under the MIT License
+http://www.tipue.com/search
+*/
+
+
+var tipuesearch_stop_words = ["and", "be", "by", "do", "for", "he", "how", "if", "is", "it", "my", "not", "of", "or", "the", "to", "up", "what", "when"];
+
